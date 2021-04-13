eaa8ccd543e9243c5fe2db7aa84f29521cbba6a3
weisui-ad <sway_2019@sina.co>
2021-04-13 21:34:30 +0800

    add task8_3 texturing

--- examples/task8/task8_2_VBO.cc
+++ examples/task8/task8_2_VBO.cc
@@ -1,6 +1,3 @@
-//
-// Created by sway on 2021/4/5.
-//
 
 //
 // Created by sway on 2021/3/11.
@@ -15,150 +12,176 @@
 #include <GL/glut.h>  // GLUT, include glu.h and gl.h
 #endif
 
-/* Global variables */
-char title[] = "3D Shapes";
 
-/* Initialize OpenGL Graphics */
-void initGL() {
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
-    glClearDepth(1.0f);                   // Set background depth to farthest
-    glEnable(GL_DEPTH_TEST);   // Enable depth testing for z-culling
-    glDepthFunc(GL_LEQUAL);    // Set the type of depth-test
-    glShadeModel(GL_SMOOTH);   // Enable smooth shading
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
+#define BUFFER_OFFSET(offset) ((char*) NULL + offset)
+#define NumberOf(array) (sizeof(array)/sizeof(array[0]))
+
+typedef struct{
+    GLfloat x, y, z;
+} vec3;
+
+typedef struct{
+    vec3 xlate; /*Translation*/
+    GLfloat angle;
+    vec3 axis;
+} XForm;
+
+//enum {Cube, NumVAOs};
+GLuint VAO;
+XForm Xform[]={
+        {{-2.0, 0.0, 0.0}, 0.0, {0.0, 1.0, 0.0}}
+};
+GLfloat angle = 0.0;
+GLsizei n_elements;
+
+// 定义属性对应的缓冲器
+enum {Vertices, Colors, Elements, NumVBOs};
+GLuint buffers[NumVBOs];
+
+
+void make_objects(){
+
+    // 创建顶点数组对象
+    glGenVertexArrays(1, &VAO);
+
+    GLfloat cubeVerts[][3]={
+            {-1.0, -1.0, -1.0},
+            {-1.0, -1.0, 1.0},
+            {-1.0, 1.0, -1.0},
+            {-1.0, 1.0, 1.0},
+            {1.0, -1.0, -1.0},
+            {1.0, -1.0, 1.0},
+            {1.0, 1.0, -1.0},
+            {1.0, 1.0,  1.0}
+    };
+
+    GLfloat cubeColors[][3]={
+            {0.0, 0.0, 0.0},
+            {0.0, 0.0, 1.0},
+            {0.0, 1.0, 0.0},
+            {0.0, 1.0, 0.0},
+            {1.0, 0.0, 0.0},
+            {1.0, 0.0, 1.0},
+            {1.0, 1.0, 0.0},
+            {1.0, 1.0, 1.0}
+    };
+
+    GLubyte cubeIndices[]={
+            0,  1,  3,  2,
+            4,  6,  7,  6,
+            2,  3,  7,  6,
+            0,  4,  5,  1,
+            0,  2,  6,  4,
+            1,  5,  7,  3
+    };
+    n_elements = NumberOf(cubeIndices);
+
+    // 绑定定点数组
+    glBindVertexArray(VAO);
+
+    // 每个属性定义一个buffer
+    glGenBuffers(NumVBOs, buffers);
+
+    // 绑定缓冲器，并绑定定点数组数据
+    // 顶点坐标
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[Vertices]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVerts), cubeVerts, GL_STATIC_DRAW);
+    glVertexPointer(3, GL_FLOAT, 0, BUFFER_OFFSET(0));
+    glEnableClientState(GL_VERTEX_ARRAY);
+
+    // 颜色
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[Colors]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeColors), cubeColors, GL_STATIC_DRAW);
+    glColorPointer(3, GL_FLOAT, 0, BUFFER_OFFSET(0));
+    glEnableClientState(GL_COLOR_ARRAY);
+
+    // 面片索引
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffers[Elements]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubeIndices), cubeIndices, GL_STATIC_DRAW);
+
+
+}
+void init(){
+
+
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glShadeModel(GL_FLAT);  // 平滑模式
+
+    // 创建物体
+    make_objects();
 }
 
-void  make_point_cloud(){
+void display(){
 
-    GLint code = glewInit();// OpenGL的扩扩展库glew中的函数，这句必须调用，否则运行不通
+    // 清除深度缓冲器和颜色缓冲器
+    glClear(GL_COLOR_BUFFER_BIT || GL_DEPTH_BUFFER_BIT);
 
+    // 3种不同的模式，分别将多边形渲染成点，线和填充的模式
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_POINTS);
+    //  glPolygonMode(GL_FRONT_AND_BACK, GL_POINTS);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
+    glPushMatrix();
+        glBindVertexArray(VAO);
+        glDrawElements(GL_QUADS, n_elements, GL_UNSIGNED_BYTE, BUFFER_OFFSET(0));
+    glPopMatrix();
 
+    glutSwapBuffers();
 }
 
-/* Handler for window-repaint event. Called back when the window first appears and
-   whenever the window needs to be re-painted. */
-void display() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
-    glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
+void reshape(int w, int h){
 
-    // Render a color-cube consisting of 6 quads with different colors
-    glLoadIdentity();                 // Reset the model-view matrix
-    glTranslatef(1.5f, 0.0f, -7.0f);  // Move right and into the screen
+    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+    // 透视投影
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60, (GLfloat)w/(GLfloat)h, 0.01, 10000.0);
 
-    glBegin(GL_QUADS);                // Begin drawing the color cube with 6 quads
-    // Top face (y = 1.0f)
-    // Define vertices in counter-clockwise (CCW) order with normal pointing out
-    glColor3f(0.0f, 1.0f, 0.0f);     // Green
-    glVertex3f( 1.0f, 1.0f, -1.0f);
-    glVertex3f(-1.0f, 1.0f, -1.0f);
-    glVertex3f(-1.0f, 1.0f,  1.0f);
-    glVertex3f( 1.0f, 1.0f,  1.0f);
-
-    // Bottom face (y = -1.0f)
-    glColor3f(1.0f, 0.5f, 0.0f);     // Orange
-    glVertex3f( 1.0f, -1.0f,  1.0f);
-    glVertex3f(-1.0f, -1.0f,  1.0f);
-    glVertex3f(-1.0f, -1.0f, -1.0f);
-    glVertex3f( 1.0f, -1.0f, -1.0f);
-
-    // Front face  (z = 1.0f)
-    glColor3f(1.0f, 0.0f, 0.0f);     // Red
-    glVertex3f( 1.0f,  1.0f, 1.0f);
-    glVertex3f(-1.0f,  1.0f, 1.0f);
-    glVertex3f(-1.0f, -1.0f, 1.0f);
-    glVertex3f( 1.0f, -1.0f, 1.0f);
-
-    // Back face (z = -1.0f)
-    glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
-    glVertex3f( 1.0f, -1.0f, -1.0f);
-    glVertex3f(-1.0f, -1.0f, -1.0f);
-    glVertex3f(-1.0f,  1.0f, -1.0f);
-    glVertex3f( 1.0f,  1.0f, -1.0f);
-
-    // Left face (x = -1.0f)
-    glColor3f(0.0f, 0.0f, 1.0f);     // Blue
-    glVertex3f(-1.0f,  1.0f,  1.0f);
-    glVertex3f(-1.0f,  1.0f, -1.0f);
-    glVertex3f(-1.0f, -1.0f, -1.0f);
-    glVertex3f(-1.0f, -1.0f,  1.0f);
-
-    // Right face (x = 1.0f)
-    glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
-    glVertex3f(1.0f,  1.0f, -1.0f);
-    glVertex3f(1.0f,  1.0f,  1.0f);
-    glVertex3f(1.0f, -1.0f,  1.0f);
-    glVertex3f(1.0f, -1.0f, -1.0f);
-    glEnd();  // End of drawing color-cube
-
-    // Render a pyramid consists of 4 triangles
-    glLoadIdentity();                  // Reset the model-view matrix
-    glTranslatef(-1.5f, 0.0f, -6.0f);  // Move left and into the screen
-
-    glBegin(GL_TRIANGLES);           // Begin drawing the pyramid with 4 triangles
-    // Front
-    glColor3f(1.0f, 0.0f, 0.0f);     // Red
-    glVertex3f( 0.0f, 1.0f, 0.0f);
-    glColor3f(0.0f, 1.0f, 0.0f);     // Green
-    glVertex3f(-1.0f, -1.0f, 1.0f);
-    glColor3f(0.0f, 0.0f, 1.0f);     // Blue
-    glVertex3f(1.0f, -1.0f, 1.0f);
-
-    // Right
-    glColor3f(1.0f, 0.0f, 0.0f);     // Red
-    glVertex3f(0.0f, 1.0f, 0.0f);
-    glColor3f(0.0f, 0.0f, 1.0f);     // Blue
-    glVertex3f(1.0f, -1.0f, 1.0f);
-    glColor3f(0.0f, 1.0f, 0.0f);     // Green
-    glVertex3f(1.0f, -1.0f, -1.0f);
-
-    // Back
-    glColor3f(1.0f, 0.0f, 0.0f);     // Red
-    glVertex3f(0.0f, 1.0f, 0.0f);
-    glColor3f(0.0f, 1.0f, 0.0f);     // Green
-    glVertex3f(1.0f, -1.0f, -1.0f);
-    glColor3f(0.0f, 0.0f, 1.0f);     // Blue
-    glVertex3f(-1.0f, -1.0f, -1.0f);
-
-    // Left
-    glColor3f(1.0f,0.0f,0.0f);       // Red
-    glVertex3f( 0.0f, 1.0f, 0.0f);
-    glColor3f(0.0f,0.0f,1.0f);       // Blue
-    glVertex3f(-1.0f,-1.0f,-1.0f);
-    glColor3f(0.0f,1.0f,0.0f);       // Green
-    glVertex3f(-1.0f,-1.0f, 1.0f);
-    glEnd();   // Done drawing the pyramid
-
-    glutSwapBuffers();  // Swap the front and back frame buffers (double buffering)
+    // 定义视图矩阵
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    // 相机默认位于原点，指向z轴负方向
+    gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 }
 
-/* Handler for window re-size event. Called back when the window first appears and
-   whenever the window is re-sized with its new width and height */
-void reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integer
-    // Compute aspect ratio of the new window
-    if (height == 0) height = 1;                // To prevent divide by 0
-    GLfloat aspect = (GLfloat)width / (GLfloat)height;
-
-    // Set the viewport to cover the new window
-    glViewport(0, 0, width, height);
-
-    // Set the aspect ratio of the clipping volume to match the viewport
-    glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
-    glLoadIdentity();             // Reset
-    // Enable perspective projection with fovy, aspect, zNear and zFar
-    gluPerspective(45.0f, aspect, 0.1f, 100.0f);
+void keyboard(unsigned char key, int x, int y){
+    switch (key) {
+        case 'd':
+            glutPostRedisplay();
+            break;
+        default:
+            glutPostRedisplay();
+            break;
+    }
 }
 
-/* Main function: GLUT runs as a console application starting at main() */
-int main(int argc, char** argv) {
-    glutInit(&argc, argv);            // Initialize GLUT
-    glutInitDisplayMode(GLUT_DOUBLE); // Enable double buffered mode
-    glutInitWindowSize(640, 480);   // Set the window's initial width & height
-    glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
-    glutCreateWindow(title);          // Create window with the given title
-    glutDisplayFunc(display);       // Register callback handler for window re-paint event
-    glutReshapeFunc(reshape);       // Register callback handler for window re-size event
-    initGL();                       // Our own OpenGL initialization
-    glutMainLoop();                 // Enter the infinite event-processing loop
+void spinDisplay(void){
+    angle = angle +2.0;
+    if (angle > 360.0) angle =  angle -360.0;
+    glutPostOverlayRedisplay();
+}
+
+void mouse (int button, int state, int x, int y){
+    switch (button) {
+        case GLUT_LEFT_BUTTON:
+            if(state == GLUT_DOWN){
+                glutIdleFunc(spinDisplay);
+            }
+            break;
+    }
+}
+int main(int argc, char*argv[]){
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow(argv[0]);
+    init();
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
+    glutMainLoop();
     return 0;
+
 }
--- examples/task8/task8_3_texturing.cc
+++ examples/task8/task8_3_texturing.cc
@@ -2,6 +2,7 @@
 // Created by sway on 2021/4/5.
 //
 
+
 int main(int argc, char *argv[]){
     return 0;
 }
